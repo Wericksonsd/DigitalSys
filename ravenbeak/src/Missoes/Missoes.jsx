7a58f917696638c5600f4330a9586d2f2e8b7a21
Werickson Dauta <105117577+Wericksonsd@users.noreply.github.com>
2024-02-15 19:42:25 -0300

    Finalll

--- ravenbeak/src/Missoes/Missoes.jsx
+++ ravenbeak/src/Missoes/Missoes.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styles2 from "./Missoes.module.css";
-import styles from "./a.module.css";
+import styles from "./Missoes.module.css";
 import { IoSkull }  from "react-icons/io5";
 import { GiCrownedSkull } from "react-icons/gi";
 
@@ -12,6 +11,7 @@ function Missoes(props){
     const [tituloMissao, setTituloMissao] = useState();
     const [textoMissao, setTextoMissao] = useState();
     const [recompensaMissao, setRecompensaMissao] = useState();
+    const [menuMss, setMenuMss] = useState(true);
 
     useEffect(() => {
         mudarNivelM(props.nivelMissao);
@@ -22,39 +22,24 @@ function Missoes(props){
 
     function mudarNivelM(i){
         
-        switch(i){
+        let nvlMss = '';
 
-            case 1:
-                document.getElementsById('nivelM1').style.display = 'block';
-            break;
-
-            case 2:
-                document.getElementById('nivelM2').style.display = 'block';
-            break;
-
-            case 3:
-                document.getElementById('nivelM3').style.display = 'block';
-            break;
-
-            case 4:
-                document.getElementById('nivelM4').style.display = 'block';
-            break;
+        for(;i>0;i--){
+            nvlMss = nvlMss + '☠'; 
         }
 
-        return;
+        setNivelMissao(nvlMss);
     }
 
+
     return(
-        <div className={styles.missoesContainer}>
-            <div className={styles.nivelGeral}>
-                <div id='nivelM1' className={styles.nivelM1}> <IoSkull/> </div>
-                <div id='nivelM2' className={styles.nivelM2}> <IoSkull/><IoSkull/> </div>
-                <div id='nivelM3' className={styles.nivelM3}> <IoSkull/><IoSkull/><IoSkull/> </div>
-                <div id='nivelM4' className={styles.nivelM4}> <IoSkull/><IoSkull/><IoSkull/><GiCrownedSkull className={styles.caveiraRei}/> </div>
+        <div onClick={() => {setMenuMss(!menuMss)}} className={menuMss ? styles.missoesContainer : styles.missoesContainerAberto}>            
+            <div className={menuMss ? styles.nivelGeral : styles.nivelGeralA}>
+                <div id='nivelM1' className={menuMss ? styles.nivelM1 : styles.nivelM1A}> {nivelMissao} </div>
             </div>
-            <div className={styles.ttl}><h2>TITULO</h2></div>
-            <div className={styles.descr}><p>historia</p></div>
-            <div className={styles.gold}><h3>00000</h3></div>
+            <div className={menuMss ? styles.ttl : styles.ttlA}><h2>{tituloMissao}</h2></div>
+            <div className={menuMss ? styles.descr : styles.descrA}><p>{textoMissao}</p></div>
+            <div className={menuMss ? styles.gold : styles.goldA}><h3>₲  {recompensaMissao}</h3></div>
         </div>
     )
 
@@ -65,7 +50,7 @@ Missoes.defaultProps = {
     nivelMissao: 1,
     tituloMissao: "Indefinida",
     textoMissao: "Indefinida",
-    recompensaMissao: "desconhecida",
+    recompensaMissao: "00000",
 }
 
 export default Missoes
